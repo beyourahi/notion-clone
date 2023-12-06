1750d8aa6a5c0ae76e33f179dce5daed81a5080d
beyourahi <beyourahi@gmail.com>
2023-12-06 09:33:32 +0600

    project setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        formats: ["image/avif", "image/webp"],
+        remotePatterns: [
+            {
+                hostname: "lh3.googleusercontent.com"
+            }
+        ]
+    }
+};
 
 module.exports = nextConfig;
