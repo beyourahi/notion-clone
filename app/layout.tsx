a283f4a48441a0b03a6197e09586d7a5733c8116
Rahi Khan <beyourahi@gmail.com>
2023-12-06 09:25:00 +0600

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Providers } from "./providers";
+import { PropsWithChildren } from "react";
+import { cn } from "lib";
+import { Inter } from "next/font/google";
+
+const font = Inter({ subsets: ["latin"], display: "swap" });
+
+export const metadata: Metadata = {
+    title: "Web Template",
+    description: "A template to build cool shit"
+};
+
+const RootLayout = ({ children }: PropsWithChildren) => (
+    <html lang="en" className="light scroll-smooth">
+        <body className={cn("antialiased", font.className)}>
+            <Providers>
+                <main>{children}</main>
+            </Providers>
+        </body>
+    </html>
+);
+
+export default RootLayout;
